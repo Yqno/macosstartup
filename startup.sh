8a4a33892f08a9c9f81b41a7e4af3e7cdc8fbd15
Y Q N O <79216939+Yqno@users.noreply.github.com>
2023-06-20 11:02:00 +0200

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,16 @@
+brew upgrade
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
+sudo spoof-mac randomize en0
